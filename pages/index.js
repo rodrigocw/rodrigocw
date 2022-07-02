bba6fe79c71d8e25a11e090d916c762cc2d31e3f
root <root@debian.rodrigocw>
2022-07-01 21:34:15 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() {
-    return <div>Bem Vindo!<br/><br/>
-    Em construção...4</div>
+    return <div>
+        <h4><strong>Quando a vida perde o sentido</strong></h4>
+        <p>&ldquo;Existem momentos na nossa vida que a vida parece n&atilde;o ter sentido, estamos vazios, nos sentindo sozinhos, n&atilde;o queremos ficar com as pessoas que amamos, choramos, nos desesperamos, o trabalho perde o sentido, os relacionamentos familiares perdem o sentido, o acordar todas as manh&atilde;s se torna uma tortura, o dia n&atilde;o passa, a velha pergunta n&atilde;o deixa de ser repetida, &ldquo;o que vim fazer neste mundo&rdquo; e pior, nada faz sentido, como se viver fosse um peso e que temos a certeza que vamos sucumbir a qualquer momento, a sensa&ccedil;&atilde;o que estamos enlouquecendo, a desconex&atilde;o gradativa do que entendemos por realidade, o sono n&atilde;o vem, e quando vem n&atilde;o nos repousa, fantasmas vem assombrar nossos sonhos, e ficamos nesta, dia ap&oacute;s dia, esperando em v&atilde;o que o pr&oacute;ximo dia seja melhor, que o final de semana vai melhorar, e pior, ainda nos cobramos porque dever&iacute;amos estar bem, estar vivendo intensamente, sermos produtivos, sermos felizes.&rdquo;</p>
+        <p style="text-align: center;">01/07/2022</p>
+    </div>
 }
 
 export default Home
